cc35d64f37954d591e479882ca9f058677a652ff
rahimialijan <alijanrahimi24@yahoo.com>
2023-02-26 19:21:37 +0500

    Completion of shop and sproduct page

--- main.js
+++ main.js
@@ -13,5 +13,22 @@ if (close){
     })
 }
 
+// Image Solid Show for sproduct page images//
+const MainImg=document.getElementById("MainImg")
+const smallImg=document.getElementsByClassName("small-img")
+
+smallImg[0].onclick=function(){
+    MainImg.src=smallImg[0].src;
+}
+smallImg[1].onclick=function(){
+    MainImg.src=smallImg[1].src;
+}
+smallImg[2].onclick=function(){
+    MainImg.src=smallImg[2].src;
+}
+smallImg[3].onclick=function(){
+    MainImg.src=smallImg[3].src;
+}
+
 
   
